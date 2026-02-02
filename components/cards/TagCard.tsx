5e649162fde2a3c8ce1972a6c81b0e9fbf4cea43
Mark Joshua Austria <austriamark.mja@gmail.com>
2026-02-02 22:46:08 +0800

    feat: add QuestionCard and Metric components, enhance routing and utility functions

--- components/cards/TagCard.tsx
+++ components/cards/TagCard.tsx
@@ -1,12 +1,15 @@
-import ROUTES from "@/constants/routes";
 import Link from "next/link";
-import { Badge } from "../ui/badge";
+import React from "react";
+
+import ROUTES from "@/constants/routes";
 import { getDeviconClassName } from "@/lib/utils";
+
+import { Badge } from "../ui/badge";
 
 interface Props {
   _id: string;
   name: string;
-  questions: number;
+  questions?: number;
   showCount?: boolean;
   compact?: boolean;
 }
