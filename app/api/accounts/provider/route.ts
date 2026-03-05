ea10f0dcd1508807c5b1d596839b0b477f72e386
Mark Joshua Austria <austriamark.mja@gmail.com>
2026-03-05 14:07:54 +0800

    feat: fix DELETE route to use Account model for account deletion and add dbConnect to POST route

--- app/api/accounts/provider/route.ts
+++ app/api/accounts/provider/route.ts
@@ -5,11 +5,14 @@ import handleError from "@/lib/handlers/error";
 import { NotFoundError, ValidationError } from "@/lib/http-errors";
 import { AccountSchema } from "@/lib/validations";
 import z from "zod";
+import dbConnect from "@/lib/mongoose";
 
 export async function POST(request: Request) {
   const { providerAccountId } = await request.json();
 
   try {
+    await dbConnect();
+
     const validatedData = AccountSchema.partial().safeParse({
       providerAccountId,
     });
