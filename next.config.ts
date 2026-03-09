23689acc5975d8235b5f9111ec10d7d8b2d1e6dd
Mark Joshua Austria <austriamark.mja@gmail.com>
2026-03-09 16:41:28 +0800

    chore: add radix-ui dependency to package.json

--- next.config.ts
+++ next.config.ts
@@ -7,10 +7,21 @@ const nextConfig: NextConfig = {
   },
   serverExternalPackages: ["pino", "pino-pretty"],
   images: {
+    qualities: [100, 75],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "static.vecteezy.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
         port: "",
       },
     ],
