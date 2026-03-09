413b0c8d3472a7cb74d1ea6c30e46bdb5f743b38
Mark Joshua Austria <austriamark.mja@gmail.com>
2026-03-09 16:41:38 +0800

    feat: implement user avatar component and update navigation to include user authentication handling

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,7 +5,6 @@ import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
-import { auth } from "@/auth";
 
 const questions = [
   {
@@ -53,10 +52,6 @@ interface SearchParams {
 }
 
 const Home = async ({ searchParams }: SearchParams) => {
-  const session = await auth();
-
-  console.log("Session: ", session);
-
   const { query = "", filter = "" } = await searchParams;
 
   const filteredQuestions = questions.filter((question) => {
