b84ee8e63bb71ea4f2e591bde8e8b2a79d0c0800
Mark Joshua Austria <austriamark.mja@gmail.com>
2026-03-18 09:20:54 +0800

    feat: implement getHotQuestions and getTopTags functions, enhance RightSidebar with DataRenderer for dynamic content display

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -15,6 +15,7 @@ import {
   IncrementViewsSchema,
   PaginatedSearchParamsSchema,
 } from "../validations";
+import dbConnect from "../mongoose";
 
 export async function createQuestion(params: CreateQuestionParams): Promise<ActionResponse<Question>> {
   const validationResult = await action({
@@ -281,6 +282,21 @@ export async function incrementViews(params: IncrementViewsParams): Promise<Acti
     return {
       success: true,
       data: { views: question.views },
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
+
+export async function getHotQuestions(): Promise<ActionResponse<Question[]>> {
+  try {
+    await dbConnect();
+
+    const questions = await Question.find().sort({ views: -1, upvotes: -1 }).limit(5);
+
+    return {
+      success: true,
+      data: JSON.parse(JSON.stringify(questions)),
     };
   } catch (error) {
     return handleError(error) as ErrorResponse;
--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -3,6 +3,7 @@ import action from "../handlers/action";
 import handleError from "../handlers/error";
 import { GetTagQuestionsSchema, PaginatedSearchParamsSchema } from "../validations";
 import { Question, Tag } from "@/database";
+import dbConnect from "../mongoose";
 
 export const getTags = async (
   params: PaginatedSearchParams
@@ -115,6 +116,21 @@ export const getTagQuestions = async (
         questions: JSON.parse(JSON.stringify(questions)),
         isNext,
       },
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+};
+
+export const getTopTags = async (): Promise<ActionResponse<Tag[]>> => {
+  try {
+    await dbConnect();
+
+    const tags = await Tag.find().sort({ questions: -1 }).limit(5);
+
+    return {
+      success: true,
+      data: JSON.parse(JSON.stringify(tags)),
     };
   } catch (error) {
     return handleError(error) as ErrorResponse;
